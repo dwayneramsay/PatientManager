378fecfd377511de03a84384b9271fe09ab2a143
dwramsay <dwramsay@JM-DWRAMSAY.digicelgroup.local>
2014-11-26 10:51:34 -0500

    Additional changes

--- BaseObjects/src/ContactInformation.java
+++ BaseObjects/src/ContactInformation.java
@@ -1,91 +1,168 @@
 import java.util.ArrayList;
+import java.lang.Exception;
+
 public class ContactInformation {
 	
-	private ArrayList<ContactDetail> ContactInformationList;
+	private ArrayList<ContactDetail> ContactDetailList;
 	
 	public ContactInformation(){
-		
-		ContactInformationList = new ArrayList<ContactDetail>();
+		ContactDetailList = new ArrayList<ContactDetail>();
+		//ContactInformationList = new ArrayList<ContactDetail>();
 		//ContactInformationList.add(new ContactDetail());
 		
 	}
 	
+	public void addContactInformation(ContactDetail.ContactDetailGroup contactDetailGroup, ContactDetail.ContactDetailType contactDetailType) 
+			throws Exception{
+		
+		switch (contactDetailGroup){
+		case HOME:
+			break;
+		case WORK:
+			break;
+		case SCHOOL:
+			break;
+		default:
+			throw new Exception();				
+		}
+		
+	 public boolean isContactDetailGroupSet(ContactDetail.ContactDetailGroup contactDetailGroup){
+		 for (int i=0; i<ContactDetailList.size(); i++){
+			 
+		 }
+	 }
+	 {
+		 
+	 }
+		
+	}
 	
-	
-	
-	private static class ContactDetail{
-		public enum ContactDetailType {ADDRESS, FIXEDLINE, MOBILE, FAX, EMAIL};
+	private abstract static class ContactDetail{
+		
+		public static enum ContactDetailGroup {HOME, WORK, SCHOOL};
+		public static enum ContactDetailType {ADDRESS, FIXEDLINE, MOBILE, FAX, EMAIL};
+		
 		private ContactDetailType contactDetailType;
-		private String[] contactDetail;
+		private ContactDetailGroup contactDetailGroup;
 		
-			
-		public ContactDetail(ContactDetailType contactDetailType, String contactGenericDetail){
+		public ContactDetail(){
+			;
+		}	
+		
+		public ContactDetail(ContactDetailGroup contactDetailGroup, ContactDetailType contactDetailType){
+			this.setContactDetailGroup(contactDetailGroup);
 			this.setContactDetailType(contactDetailType);
-			this.contactDetail = new String[1];
-			this.contactDetail[0] = contactGenericDetail;
-		}
-		
-		public ContactDetail(ContactDetailType contactDetailType, String[] contactGenericDetail){
-			this.setContactDetailType(contactDetailType);
-			this.contactDetail = new String[5];
-			
-			
-			this.contactDetail[0] = contactGenericDetail[0];
-			this.contactDetail[1] = contactGenericDetail[1];
-			this.contactDetail[2] = contactGenericDetail[2];
-			this.contactDetail[3] = contactGenericDetail[3];
-			this.contactDetail[4] = contactGenericDetail[4];
-		}
-		
+		}			
+				
 				
 		private void setContactDetailType(ContactDetailType contactDetailType){
 			this.contactDetailType = contactDetailType;
-		}
-		
+		}		
 
 		public ContactDetailType getContactDetailType(){
 			return this.contactDetailType;
-		}
+		}		
 		
-				public boolean isValidateContactDetail(){
+		public ContactDetailGroup getContactDetailGroup() {
+			return contactDetailGroup;
+		}
+
+		public void setContactDetailGroup(ContactDetailGroup contactDetailGroup) {
+			this.contactDetailGroup = contactDetailGroup;
+		}
+
+		public abstract boolean isValidateContactDetail();
+		
+	}
+	
+	private class ContactAddressDetail extends ContactDetail{
+		private String addressLine1;
+		private String addressLine2;
+		private String cityTown;
+		private String parishState;
+		private String country;
+		private String postalCode;		
+		
+		public ContactAddressDetail(ContactDetailGroup contactDetailGroup, ContactDetailType contactDetailType){
+			super(contactDetailGroup, contactDetailType);
 			
-			switch (this.getContactDetailType()){
-				case ADDRESS : 
-					return this.isValidAddress();
-				
-				case FIXEDLINE : 
-				case MOBILE : 
-				case FAX : 
-					return this.isValidTelephoneNumber();
-				
-				case EMAIL : 
-					return this.isValidEmailAddress();
-					
-				default:
-					return false;
-					
-					
-			}
+		}
+		
+		public String getAddressLine1() {
+			return addressLine1;
+		}
+		
+		public void setAddressLine1(String addressLine1) {
+			this.addressLine1 = addressLine1;
+		}
+		
+		public String getAddressLine2() {
+			return addressLine2;
+		}
+		
+		public void setAddressLine2(String addressLine2) {
+			this.addressLine2 = addressLine2;
+		}
+
+		public String getCityTown() {
+			return this.cityTown;
+		}
+		public void setCityTown(String cityTown) {
+			this.cityTown = cityTown;
+		}
+		
+		public String getParishState() {
+			return parishState;
+		}
+		
+		public void setParishState(String parishState) {
+			this.parishState = parishState;
+		}
+		
+		public String getCountry() {
+			return country;
+		}
+		
+		public void setCountry(String country) {
+			this.country = country;
+		}
+		public String getPostalCode() {
+			return postalCode;
+		}
+		public void setPostalCode(String postalCode) {
+			this.postalCode = postalCode;
+		}
+
+		public boolean isValidateContactDetail(){
+			return false;
+		}		
+		
+		
+	}
+	
+	private class ContactTelephoneDetail extends ContactDetail{
+		
+		private String telephoneNumber;
+		
+		public ContactTelephoneDetail(ContactDetailGroup contactDetailGroup, ContactDetailType contactDetailType){
+			super(contactDetailGroup, contactDetailType);
 			
+		}		
+
+		public String getTelephoneNumber() {
+			return telephoneNumber;
 		}
-				
-		public String[] getContactDetail(){
-			return this.contactDetail;
+
+		public void setTelephoneNumber(String telephoneNumber) {
+			this.telephoneNumber = telephoneNumber;
 		}
 		
-		private boolean isValidAddress(){
+		public boolean isValidateContactDetail(){
 			return false;
-		}
+		}	
 		
-		private boolean isValidTelephoneNumber(){
-			return false;
-		}
-		
-		private boolean isValidEmailAddress(){
-			return false;
-		}
-		
-		
-	}	
+	}
+	
+	
 	
 }
